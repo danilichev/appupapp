265dc90cfe95d16893de86138e076ef5b4e8f50c
danilichev <v.danilichev@gmail.com>
2025-03-10 10:59:30 +0200

    Add base go api

---
+++ apps/api/go.mod
@@ -0,0 +1,10 @@
+module api
+
+go 1.23
+
+require github.com/danielgtaylor/huma/v2 v2.30.0
+
+require (
+	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+)
---
+++ apps/api/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/danielgtaylor/huma/v2"
+
+	"github.com/danielgtaylor/huma/v2/adapters/humago"
+	_ "github.com/danielgtaylor/huma/v2/formats/cbor"
+	"github.com/danielgtaylor/huma/v2/humacli"
+)
+
+type Options struct {
+	Port int `help:"Port to listen on" short:"p" default:"8888"`
+}
+
+type GreetingOutput struct {
+	Body struct {
+		Message string `json:"message" example:"Hello, world!" doc:"Greeting message"`
+	}
+}
+
+type ReviewInput struct {
+	Body struct {
+		Author string `json:"author" maxLength:"10" doc:"Author of the review"`
+		Rating int `json:"rating" minimum:"1" maximum:"5" doc:"Rating from 1 to 5"`
+		Message string `json:"message,omitempty" maxLength:"100" doc:"Review message"`
+	}
+}
+
+func addRoutes(api huma.API) {
+	huma.Register(api, huma.Operation{
+		OperationID: "get-greeting",
+		Method: http.MethodGet,
+		Path: "/greeting/{name}",
+		Summary: "Get a greeting",
+		Description: "Get a greeting for a person by name.",
+		Tags: []string{"Greetings"},
+	}, func(ctx context.Context, input *struct{
+		Name string `path:"name" maxLength:"30" example:"world" doc:"Name to greet"`
+	}) (*GreetingOutput, error) {
+		resp := &GreetingOutput{}
+		resp.Body.Message = fmt.Sprintf("Hello, %s!", input.Name)
+		return resp, nil
+	})
+
+	huma.Register(api, huma.Operation{
+		OperationID: "post-review",
+		Method: http.MethodPost,
+		Path: "/reviews",
+		Summary: "Post a review",
+		Tags: []string{"Reviews"},
+		DefaultStatus: http.StatusCreated,
+	}, func(ctx context.Context, i *ReviewInput) (*struct{}, error) {
+		// TODO: save review in data store.
+		return nil, nil
+	})
+}
+
+func main() {
+	cli := humacli.New(func(hooks humacli.Hooks, options *Options) {
+		router := http.NewServeMux()
+		api := humago.New(router, huma.DefaultConfig("My API", "1.0.0"))
+
+		addRoutes(api)
+
+		hooks.OnStart(func() {
+			fmt.Printf("Starting server on port %d...\n", options.Port)
+			http.ListenAndServe(fmt.Sprintf(":%d", options.Port), router)
+		})
+	})
+
+	cli.Run()
+}
