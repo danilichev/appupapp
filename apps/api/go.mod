1da4ab5b1ec864ae0240cb921bee9a2c2b9c1a51
danilichev <v.danilichev@gmail.com>
2025-03-23 10:51:39 +0200

    Serve openapi spec

--- apps/api/go.mod
+++ apps/api/go.mod
@@ -3,6 +3,7 @@ module apps/api
 go 1.23.6
 
 require (
+	github.com/MarceloPetrucio/go-scalar-api-reference v0.0.0-20240521013641-ce5d2efe0e06
 	github.com/getkin/kin-openapi v0.127.0
 	github.com/jackc/pgx/v5 v5.7.2
 	github.com/joho/godotenv v1.5.1
--- apps/api/internal/server/routes.go
+++ apps/api/internal/server/routes.go
@@ -1,17 +1,29 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/MarceloPetrucio/go-scalar-api-reference"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
 	e := echo.New()
+
+	// Register middleware
+	s.registerMiddleware(e)
+
+	// Register routes
+	s.registerRoutes(e)
+
+	return e
+}
+
+func (s *Server) registerMiddleware(e *echo.Echo) {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins:     []string{"https://*", "http://*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
@@ -19,12 +31,28 @@ func (s *Server) RegisterRoutes() http.Handler {
 		AllowCredentials: true,
 		MaxAge:           300,
 	}))
+}
 
+func (s *Server) registerRoutes(e *echo.Echo) {
 	e.GET("/", s.HelloWorldHandler)
-
+	e.GET("/docs", s.docsHandler)
 	e.GET("/health", s.healthHandler)
+}
 
-	return e
+func (s *Server) docsHandler(c echo.Context) error {
+	htmlContent, err := scalar.ApiReferenceHTML(&scalar.Options{
+		SpecURL: "./openapi.yaml",
+		CustomOptions: scalar.CustomOptions{
+			PageTitle: "Blog API",
+		},
+	})
+
+	if err != nil {
+		fmt.Printf("%v", err)
+		return c.String(http.StatusInternalServerError, "Error generating API reference")
+	}
+
+	return c.HTML(http.StatusOK, htmlContent)
 }
 
 func (s *Server) HelloWorldHandler(c echo.Context) error {
